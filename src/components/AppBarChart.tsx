be56273e2aa72dd39fad56dc7c7dc620cfd1df3f
Devansh27112001 <dkansara2711@gmail.com>
2025-06-17 19:40:56 -0400

    shadecn --charts

--- src/components/AppBarChart.tsx
+++ src/components/AppBarChart.tsx
@@ -1,22 +1,29 @@
 "use client";
-import { ChartContainer, type ChartConfig } from "@/components/ui/chart";
-import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import {
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from "@/components/ui/chart";
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
 import { chartData } from "@/lib/shadeCnUtils";
 
 const chartConfig = {
   desktop: {
     label: "Desktop",
-    color: "#2563eb",
+    color: "var(--chart-1)",
   },
   mobile: {
     label: "Mobile",
-    color: "#60a5fa",
+    color: "var(--chart-4)",
   },
 } satisfies ChartConfig;
 const AppBarChart = () => {
   return (
     <div className="">
-      <h1>Total Revenue</h1>
+      <h1 className="text-lg font-medium mb-6">Total Revenue</h1>
       <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
         <BarChart accessibilityLayer data={chartData}>
           <CartesianGrid vertical={false} />
@@ -25,8 +32,11 @@ const AppBarChart = () => {
             tickLine={false}
             tickMargin={10}
             axisLine={false}
-            tickFormatter={(value) => value.slice(0, 3)}
+            tickFormatter={(value: string) => value.slice(0, 3)}
           />
+          <YAxis tickLine={false} tickMargin={10} axisLine={false} />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <ChartLegend content={<ChartLegendContent />} />
           <Bar dataKey={"desktop"} fill="var(--color-desktop)" radius={4} />
           <Bar dataKey={"mobile"} fill="var(--color-mobile)" radius={4} />
         </BarChart>
